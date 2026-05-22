ade92815fbb60625c4dcfd6934229e916a25c0b5
ngaboelysee <ngaboelysee117@gmail.com>
2026-05-22 10:13:06 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-quant-trade-engine.vercel.app"],
+    allow_origins=[
+    "https://ai-quant-trade-engine.vercel.app",
+    "https://myapp-hmksnzqkd-nrelysee-s-projects1.vercel.app",
+    "http://localhost:3000"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
